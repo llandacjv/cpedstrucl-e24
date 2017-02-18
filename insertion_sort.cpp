1df2e58cb22e7721ea09ebcbdacd036bfcb7bb5c
Carl Joshua Llanda <joshuallanda00@gmail.com>
2017-02-18 11:44:02 +0800

    Create insertion_sort.cpp

---
+++ insertion_sort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{	
+	int arr[] = {40,30,20,50,10};
+	int n=5, j, temp;
+ 
+ 	for (int i=0;i<n;i++)
+	{
+		j=i;
+		while (j>0&&arr[j]<arr[j-1])
+		{
+			  temp     = arr[j];
+			  arr[j]   = arr[j-1];
+			  arr[j-1] = temp;
+			  j--;
+		}
+		for(int k=0;k<n;k++)
+		cout<<arr[k]<<" ";
+		cout<<endl;
+	}
+}

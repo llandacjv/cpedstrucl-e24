09a966f8d7e1f233227b6968dc34f32dd9b12228
Carl Joshua Llanda <joshuallanda00@gmail.com>
2017-02-18 12:43:24 +0800

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
 using namespace std;
  
-int main() 
-{	
+int main() {	
 	int arr[] = {30,40,20,50,10};
 	int n=5, p;
  
-	for(int i=0;i<n;i++) 
-	{
+	for(int i=0;i<n;i++) {
 		int min=arr[i], index=i;
-		for(int j=i+1;j<n;j++)
-   		{
-			if(arr[j]<min)
-			{
-				min   = arr[j];
+		for(int j=i+1;j<n;j++) {
+			if(arr[j]<min) {
+			min   = arr[j];
        			index = j;
+    			}
     		}
-    	}
     	arr[index] = arr[i];
     	arr[i] = min;
     	

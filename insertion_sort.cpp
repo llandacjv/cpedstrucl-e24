4bc856ae13971fe33bef9cd6a066a1cb8ddb3bef
Carl Joshua Llanda <joshuallanda00@gmail.com>
2017-02-18 12:44:10 +0800

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() 
-{	
+int main() {	
 	int arr[] = {40,30,20,50,10};
-	int n=5, j, temp;
+	int n=5, p, j, temp;
  
- 	for (int i=0;i<n;i++)
-	{
+ 	for(int i=0;i<n;i++) {
 		j=i;
-		while (j>0&&arr[j]<arr[j-1])
-		{
+		while(j>0&&arr[j]<arr[j-1]) {
 			  temp     = arr[j];
 			  arr[j]   = arr[j-1];
 			  arr[j-1] = temp;
 			  j--;
 		}
-		for(int k=0;k<n;k++)
+		p++;
+    	cout<<"Pass: "<<p<<": ";
+    	for(int k=0;k<n;k++)
 		cout<<arr[k]<<" ";
 		cout<<endl;
 	}
